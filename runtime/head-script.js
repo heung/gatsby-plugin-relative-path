beca926f27c4d0a34055677d6c9922e0822c467d
heung <heung.lee@gmail.com>
2023-09-04 11:27:24 -0400

    remove comments

--- runtime/head-script.js
+++ runtime/head-script.js
@@ -1,8 +1,6 @@
 'use strict';
 
 (function () {
-    // var ipfsPathRegExp = /^(\/(?:ipfs|ipns)\/[^/]+)/;
-    var ipfsPathPrefix = window.location.pathname.replace(/\/$/, '');
-
-    window.__GATSBY_IPFS_PATH_PREFIX__ = ipfsPathPrefix;
+    var i = window.location.pathname.replace(/\/$/, '');
+    window.__GATSBY_IPFS_PATH_PREFIX__ = i;
 })();
