7adb9a662e273f9e38fca101074bebc57a66f449
heung <heung.lee@gmail.com>
2023-09-04 11:21:49 -0400

    edit to allow non ipfs

--- runtime/head-script.js
+++ runtime/head-script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 (function () {
-    var ipfsPathRegExp = /^(\/(?:ipfs|ipns)\/[^/]+)/;
-    var ipfsPathPrefix = (window.location.pathname.match(ipfsPathRegExp) || [])[1] || '';
+    // var ipfsPathRegExp = /^(\/(?:ipfs|ipns)\/[^/]+)/;
+    var ipfsPathPrefix = window.location.pathname.replace(/\/$/, '');
 
     window.__GATSBY_IPFS_PATH_PREFIX__ = ipfsPathPrefix;
 })();
